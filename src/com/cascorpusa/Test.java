bc965a9b1ee05dcab9932265c2f2e1eb054a0213
Mike Rains <spockmonster@gmail.com>
2016-12-06 10:32:24 -0600

    Test class added

---
+++ src/com/cascorpusa/Test.java
@@ -0,0 +1,21 @@
+package com.cascorpusa;
+
+/**
+ * Created by Mike Rains on 12/6/2016.
+ * But ya are stuck in that chair Blanche.
+ */
+public class Test {
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+
+    public void Go() {
+        this.description = "KIRK";
+    }
+}
