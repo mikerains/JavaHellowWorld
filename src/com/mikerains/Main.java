416c2f8bfecc7e80ad800045a8c528c0c1cc1b7b
Mike Rains <spockmonster@gmail.com>
2016-12-06 10:17:21 -0600

    added description property

--- src/com/mikerains/Main.java
+++ src/com/mikerains/Main.java
@@ -2,7 +2,18 @@ package com.mikerains;
 
 public class Main {
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
     public static void main(String[] args) {
 	// write your code here
+        String name = "SPOCK";
+
     }
 }
